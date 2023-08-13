7206782e599b14fdcffeec3bed6ac18236a9ff34
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-08-14 04:19:55 +0530

    updated navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import './Navbar.css';
+import { AiFillHome,AiFillCar } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import { RiContactsBookFill } from "react-icons/ri";
+import "../index.css"
+import { useAuth0 } from "@auth0/auth0-react";
+
+
+const Navbar = () => {
+  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
+  const {user,isAuthenticated} = useAuth0();
+  return (
+    <div>
+      <nav classname="">
+
+        <div className='flex justify-between items-center m-5 pl-[30px] pr-[30px] shadow-2xl w-[95%] h-[80px] rounded-[13px]'>
+        <a href='/'><img src="../logo.jpg" className="h-[79px] " alt="logo" /></a>
+
+
+
+        <ul className="nav-menu flex gap-9">
+            <li className=''>
+               <a href='/' className='value flex items-center gap-2'><AiFillHome/>Home</a> 
+            </li>
+            <li className=''>
+               <a href='/' className='value flex items-center gap-2'><AiFillCar/>Travel</a> 
+            </li>
+            <li className=''>
+               <a href='/' className='value flex items-center gap-2'><BsFillInfoCircleFill/>About Us</a> 
+            </li>
+            <li className=''>
+               <a href='/' className='value flex items-center gap-2'><RiContactsBookFill/>Contact Us</a> 
+            </li>
+            { isAuthenticated && <div className='user'>
+          <p className='user_detail'>{user.name}</p>
+        </div>
+       }
+       
+       
+       <div>
+        {
+          
+          isAuthenticated ?<button className='btnLogin border-2 border-black p-[2px] pl-[4px] pr-[4px] rounded-[5px] hover:bg-black hover:text-white transition-all duration-200  ' onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</button> :
+          <button className='btnLogin' onClick={() => loginWithRedirect()}>Log In</button>
+          }
+          </div>
+        </ul>
+        </div>
+
+      </nav>
+    </div>
+  )
+}
+export default Navbar
