928b77a208130c4cde7f40cc6c9d72d85a90aea6
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-08-14 04:19:05 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import"./index.css";
 
+
 const App = () => {
-  return <div className="text-5xl text-center">App</div>;
+  return <div className="text-5xl text-center"></div>;
+ 
 };
 
 export default App;
