ec9195b09b47cf0f9ae38ff397e38b593d61c651
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-08-16 04:45:25 +0530

    updated Travel.js

--- src/routes/Travel.js
+++ src/routes/Travel.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Parent from '../components/Parent';
+import "../index.css"
 
 const Travel = () => {
   return (
     <div>
+      <Navbar></Navbar>
+      <Parent 
+      
+    >
+      </Parent>
       Travel
     </div>
   )
