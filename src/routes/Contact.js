8102fb6255c9f10727500bf393191a6821d24d92
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-08-16 04:45:11 +0530

    Updated contact.js

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Navbar from '../components/Navbar';
+import Parent from '../components/Parent';
+import "../index.css"
+
 
 const Contact = () => {
   return (
     <div>
+       <Navbar />
+      <Parent className="h-[70vh]"
+      >
+      </Parent>
       Contact
     </div>
   )
