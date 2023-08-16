380cf96a3811170082cb2b2c8a714d137c7bd725
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-08-16 16:32:05 +0530

    Updated Travel.js

--- src/routes/Travel.js
+++ src/routes/Travel.js
@@ -2,13 +2,18 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Parent from '../components/Parent';
 import "../index.css"
+import image from "../asset/image.jpg"
 
 const Travel = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Parent 
-      
+      cName="travel"
+      heroImg={image}
+      heading={"Hot Locations"}
+      Text={""}
+
     >
       </Parent>
       Travel
