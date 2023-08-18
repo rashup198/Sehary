4e949452119365770241d9d1ba2a761c3b88d8b9
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-08-19 04:00:02 +0530

    updated Error.js

---
+++ src/routes/Error.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Error = () => {
+  return (
+    <div>
+        <Navbar></Navbar>
+
+        <h1 className='text-center text-[60px] font-extrabold mt-[300px] text-black'>404 Error</h1>
+        <h1 className='text-center text-[40px] font-bold text-black'>Page Not Found</h1>
+
+      
+    </div>
+  )
+}
+
+export default Error
