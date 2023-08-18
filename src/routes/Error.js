a845f05246f31d0e486b8a88903f8add213c30c4
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-08-19 04:03:23 +0530

    updated Error.js

--- src/routes/Error.js
+++ src/routes/Error.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const Error = () => {
   return (
     <div>
         <Navbar></Navbar>
 
-        <h1 className='text-center text-[60px] font-extrabold mt-[300px] text-black'>404 Error</h1>
-        <h1 className='text-center text-[40px] font-bold text-black'>Page Not Found</h1>
+        <h1 className='text-center text-[60px] font-extrabold mt-[200px] text-black'>404 Error</h1>
+        <h1 className='text-center text-[40px] font-bold text-black mb-[200px]'>Page Not Found</h1>
 
-      
+      <Footer></Footer>
     </div>
   )
 }
