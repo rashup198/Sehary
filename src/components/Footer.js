1434b2d78d314b5babd9965c8a05ca00330fabec
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-08-23 16:57:12 +0530

    updated footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,21 @@
-import React from 'react'
-// import logo from "../asset/Icon 2.png"
-import { BsTwitter,BsLinkedin,BsInstagram,BsFacebook,BsYoutube } from "react-icons/bs";
-import "../index.css"
+import React from 'react';
+import { BsTwitter, BsLinkedin, BsInstagram, BsFacebook, BsYoutube } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 
 
 const Footer = () => {
   return (
-    <footer className='bg-black -mb-[100px]'>
+    <footer className='bg-black -mb-100'>
+      <div className='upper flex flex-col md:flex-row  justify-between ml-4 md:ml-100 mr-4 md:mr-100 mt-4 md:mt-[50px] md:mb-[50px] md:p-[50px] mb-4 md:mb-50 items-center'>
 
-        <div className='upper flex justify-between ml-[100px] mr-[100px] mt-[50px] mb-[50px] items-center '>
-        <div className='flex flex-col '>
-            <h1 className='txt text-[70px] font-extrabold text-white'>Sehary</h1>
-            <p className='text-[20px] text-white font-light'>Sehary provides you with the best travel experience.</p>
-        </div>
-        <div className='flex gap-[20px]'>
+      <div>
+
+      <h1 className='txt text-[70px] font-extrabold text-white'>Sehary</h1>
+      <p className='text-[20px] text-white font-light'>Sehary provides you with the best travel experience.</p> 
+
+      </div>
+
+      <div className='flex gap-[20px] mt-[20px] mb-[20px]'>
             <a href='/' target='blank'><BsTwitter className="bg-white text-3xl rounded-[4px] pl-[2px] pr-[2px] hover:transform hover:scale-110 transition-all duration-150"></BsTwitter></a>
             <a href='https://www.linkedin.com/company/sehary/' target='blank'><BsLinkedin className="bg-black  pl-[2px] pr-[2px] text-3xl text-white rounded-[4px]  hover:transform hover:scale-110 transition-all duration-150"></BsLinkedin></a>
             <a href='/'><BsInstagram className="bg-white text-3xl pl-[2px] pr-[2px] rounded-[4px]  hover:transform hover:scale-110 transition-all duration-150"></BsInstagram></a> 
@@ -22,34 +23,44 @@ const Footer = () => {
             <a href='/'><BsYoutube className="bg-white text-3xl pl-[2px] pr-[2px] rounded-[4px]  hover:transform hover:scale-110 transition-all duration-150"></BsYoutube></a>
              
         </div>
-        </div>
-        
 
-        <div className='lower ml-[100px] mr-[100px] mb-[50px]'>
-            <div className='flex justify-between'>
-                <div>
-            <h1 className='text-white text-[30px] font-extrabold'>Navigation</h1>
+      </div>
 
-            <ul className=' pb-[50px] '>
-            <li className='mt-[8px]'>
-               <Link to="/" className=' hover:underline underline-offset-2 text-center text-white'>Home</Link> 
-            </li>
-            <li className='mt-[8px]'>
-               <Link to="/travel" className=' hover:underline underline-offset-2 text-white '>Travel</Link> 
-            </li>
-            <li className='mt-[8px]'>
-               <Link to='/about' className=' hover:underline underline-offset-2 text-white'>About Us</Link> 
-            </li>
-            <li className='mt-[8px]'>
-               <Link to='/contact' className='hover:underline underline-offset-2 text-white'>Contact Us</Link> 
-            </li>
-            </ul> 
-            </div>  
 
-            <div>
-            <h1 className='text-white text-[30px] font-extrabold'>Services</h1>
 
-            <ul className=' pb-[50px] '>
+      <div className='lower ml-4 md:ml-100 mr-4 md:mr-100 mb-4'>
+        <div className='flex gap-6 ml-[20px] flex-col md:flex-row md:space-x-6 lg:space-x-12'>
+          <div className='md:w-1/4'>
+            <h1 className='text-white text-[30px] md:text-2xl font-extrabold mb-3'>
+              Navigation
+            </h1>
+            <ul>
+              <li className='mt-2'>
+                <Link to='/' className='text-white hover:underline md:hover:underline underline-offset-2'>
+                  Home
+                </Link>
+              </li>
+              <li className='mt-2'>
+                <Link to='/travel' className='text-white hover:underline'>
+                  Travel
+                </Link>
+              </li>
+              <li className='mt-2'>
+                <Link to='/about' className='text-white hover:underline'>
+                  About Us
+                </Link>
+              </li>
+              <li className='mt-2'>
+                <Link to='/contact' className='text-white hover:underline'>
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className='md:w-1/4'>
+          <h1 className='text-white text-[30px] font-extrabold'>Services</h1>
+
+            <ul className=''>
             <li className='mt-[8px]'>
                <Link to="/" className=' hover:underline underline-offset-2 text-center text-white'>Weekend Gateway</Link> 
             </li>
@@ -62,13 +73,13 @@ const Footer = () => {
             <li className='mt-[8px]'>
                <Link to='/' className='hover:underline underline-offset-2 text-white'>Sehary Finance</Link> 
             </li>
-            </ul>
-            </div>
+            </ul>  
+          </div>
 
-            <div>
-            <h1 className='text-white text-[30px] font-extrabold'>Product</h1>
+          <div className='md:w-1/4'>
+          <h1 className='text-white text-[30px] font-extrabold'>Product</h1>
 
-            <ul className=' pb-[50px] '>
+            <ul className=' '>
             <li className='mt-[8px]'>
                <Link to="/" className=' hover:underline underline-offset-2 text-center text-white'>Testimonials</Link> 
             </li>
@@ -81,15 +92,15 @@ const Footer = () => {
             <li className='mt-[8px]'>
                <Link to='/' className='hover:underline underline-offset-2 text-white'>Why Sehary?</Link> 
             </li>
-            </ul>
-            </div>
+            </ul>        
+          </div>
 
-            <div>
-            <h1 className='text-white text-[30px] font-extrabold'>Other</h1>
+          <div className='md:w-1/4'>
+          <h1 className='text-white text-[30px] font-extrabold'>Other</h1>
 
-            <ul className=' pb-[50px] '>
+            <ul className=''>
             <li className='mt-[8px]'>
-                <a href='https://wa.me/+918889947761' target='blank' class="hover:underline underline-offset-2 text-white ">Contact us</a>
+               <a href='https://wa.me/+918889947761' target='blank' class="hover:underline underline-offset-2 text-white ">Contact us</a>
             </li>
             <li className='mt-[8px]'>
                <Link to="/" className=' hover:underline underline-offset-2 text-white '>Career</Link> 
@@ -101,16 +112,16 @@ const Footer = () => {
                <Link to='/' className='hover:underline underline-offset-2 text-white'>Privacy Policy</Link> 
             </li>
             </ul>
-            </div>
-
-            </div>
-            <div>
-                <h1 className='text-center text-white'>Copyright ©2023 Sehary, All rights reserved</h1>
-            </div>
+          </div>
         </div>
-      
+        <div>
+          <h1 className='text-center text-white mt-6'>
+            Copyright © 2023 Sehary, All rights reserved
+          </h1>
+        </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
