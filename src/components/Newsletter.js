c730d43a251c1b0967355eb1c81ea634a9346882
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-08-23 13:39:01 +0530

    updated Newsletter.js

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,26 +1,44 @@
-import React from 'react'
-
+import React from 'react';
+import "./Newsletter.css"
 
 const Newsletter = () => {
-   function handlebtn(){
-    
+  function handlebtn() {
+    // Your button click logic here
   }
+
   return (
-    <div className='flex bg-[#4657f5] justify-between items-center pb-[20px] -mb-[50px] pt-[20px]'>
-        <div className='ml-[100px] '>
-            <h1 className='text-[40px] text-white '>Subscribe Newsletter</h1>
-            <p className='text-[20px] text-white mt-[10px]'>Get e-mail updates about latest updates and special offers.</p>
-        </div>
-        <div className='mr-[100px]'>
+    <div className='container flex bg-[#4657f5] justify-between items-center pb-[20px] -mb-[50px] pt-[20px]'>
+      <div className='textConatiner ml-[100px]'>
+        <h1 className='Title text-[40px] text-white'>
+          Subscribe Newsletter
+        </h1>
+        <p className='description text-[20px] text-white mt-[10px]'>
+          Get e-mail updates about latest updates and special offers.
+        </p>
+      </div>
 
-            <form method="POST" action='https://getform.io/f/01bb9c33-6bd0-4585-be4d-efc169d667be'>
-            <input type='email' name='newsLetter' placeholder="Enter your email address" className=' bg-white border p-2 w-[300px] rounded-l-md' ></input>
-            
-            <button className='p-2 border border-white text-white rounded-r-md' onClick={handlebtn}>Subscribe</button>
-            </form>
-        </div>
+      <div className='mr-[100px]'>
+      <form
+        method='POST'
+        action='https://getform.io/f/01bb9c33-6bd0-4585-be4d-efc169d667be'
+        className='from'
+      >
+        <input
+          type='email'
+          name='newsLetter'
+          placeholder='Enter your email address'
+          className='inputbox  bg-white border p-2 w-[300px] rounded-l-md'
+        />
+        <button
+          className='btn p-2 border border-white text-white rounded-r-md'
+          onClick={handlebtn}
+        >
+          Subscribe
+        </button>
+      </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Newsletter
+export default Newsletter;
