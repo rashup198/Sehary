f06bc1829df4a12652437db7e846c27fe82d9336
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-09-04 22:31:21 +0530

    updated

---
+++ SeharyAdminDashboard/dashboard/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css';
+// import { BrowserRouter as Router,Route } from "react-router-dom";
+// import Location from './Routes/Location';
+// import Employees from './Routes/Employees';
+
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+        app
+            <Navbar />
+      
+    </div>
+  );
+}
+
+export default App;
