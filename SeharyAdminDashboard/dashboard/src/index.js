eeb39cc21ccd0d770e70463c54d9b726f64cdff7
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-09-05 05:34:37 +0530

    updated index.js

--- SeharyAdminDashboard/dashboard/src/index.js
+++ SeharyAdminDashboard/dashboard/src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Location from './Routes/Location';
+import Employees from './Routes/Employees';
+import Statistics from './Routes/Statistics';
+import Home from './Routes/Home';
 import App from './App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <App />
+    <BrowserRouter>
+    <App></App>
+    <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/location" element={<Location></Location>} />
+        <Route path="/employees" element={<Employees></Employees>} />
+        <Route path="/statistics" element={<Statistics></Statistics>} />
+       
+      
+    
+    </Routes>
+    </BrowserRouter>
   
 );
 
