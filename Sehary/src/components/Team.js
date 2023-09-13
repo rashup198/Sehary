9f7cd2d906b4d6d0c138d88a645badd4050a77db
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-09-13 15:50:11 +0530

    updated Team

--- Sehary/src/components/Team.js
+++ Sehary/src/components/Team.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import TeamCard from './TeamCard';
+import naveen from "../asset/naveen.png"
+import chandu from '../asset/Chandu.jpg'
+import vihari from "../asset/vihari.png"
+import rohit from '../asset/Rohit.jpg'
 
 const Team = () => {
   const responsive = {
@@ -26,41 +30,33 @@ const Team = () => {
   const dataTeam = [
     {
       id: 1,
-      image: 'https://i.ibb.co/0jZzQYH/IMG-20210102-123456.jpg',
+      image: {naveen},
       name: 'Naveen Anand',
-      url: 'https://www.linkedin.com/in/rahul-vihari-9b0b0a1b4/',
+      url: 'https://www.linkedin.com/in/naveen-anand-733022283/?trk=contact-info',
       designation: 'Co-Founder',
       exp: 'Serial Entrepreneur | Trader',
     },
     {
       id: 2,
-      image: 'https://i.ibb.co/0jZzQYH/IMG-20210102-123456.jpg',
+      image: {vihari},
       name: 'Vihari',
-      url: 'https://www.linkedin.com/in/rahul-vihari-9b0b0a1b4/',
+      url: 'https://www.linkedin.com/in/vihari09/?trk=contact-info',
       designation: 'Co-Founder',
       exp: 'Serial Entrepreneur | Trader',
     },
     {
       id: 3,
-      image: 'https://i.ibb.co/0jZzQYH/IMG-20210102-123456.jpg',
+      image: {chandu},
       name: 'Chandra Sekhar',
-      url: 'https://www.linkedin.com/in/rahul-vihari-9b0b0a1b4/',
+      url: 'https://www.linkedin.com/in/rahul-vihari-9b0b0a1b4/https://www.linkedin.com/in/k-s-chandra-sekhar-lanka-682963bb/',
       designation: 'CTO',
       exp: 'Serial Entrepreneur | Trader',
     },
     {
       id: 4,
-      image: 'https://i.ibb.co/0jZzQYH/IMG-20210102-123456.jpg',
+      image: {rohit},
       name: 'Rohit Tota',
-      url: 'https://www.linkedin.com/in/rahul-vihari-9b0b0a1b4/',
-      designation: 'CEO',
-      exp: 'Serial Entrepreneur | Trader',
-    },
-    {
-      id: 5,
-      image: 'https://i.ibb.co/0jZzQYH/IMG-20210102-123456.jpg',
-      name: 'Rahul',
-      url: 'https://www.linkedin.com/in/rahul-vihari-9b0b0a1b4/',
+      url: 'https://www.linkedin.com/in/rohith-thota-140b38224/?trk=contact-info',
       designation: 'CEO',
       exp: 'Serial Entrepreneur | Trader',
     },
