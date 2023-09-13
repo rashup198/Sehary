f6039c8e75f1f2f90e6b7d9bd3b2187ce0266f37
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-09-13 15:49:01 +0530

    updated DestinationData.js

--- Sehary/src/components/DestinationData.js
+++ Sehary/src/components/DestinationData.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react'
-import { FaMapMarker } from "react-icons/fa";
+import React from 'react';
+import { FaMapMarker } from 'react-icons/fa';
 
-
-function DestinationData(props){
+function DestinationData(props) {
   return (
     <>
-    
-    <div className='destiCard flex flex-col w-[32%] p-[5px] text-start shadow-2xl rounded-[7px] cursor-pointer '>
-      <div className='destiImage h-[200px] overflow-hidden rounded-[7px] '>
-        <img alt='card-image' src={props.destiImage} className='w-[100%] h-[100%] transition-all duration-150 ease-in-out rounded-[7px] hover:transform hover:scale-[1.2]'></img>
+      <div className='destiCard flex flex-col w-full lg:w-[32%] p-2 lg:p-5 text-start shadow-2xl rounded-[7px] cursor-pointer'>
+        <div className='destiImage h-[200px] overflow-hidden rounded-[7px]'>
+          <img
+            alt='card-image'
+            src={props.destiImage}
+            className='w-full h-[100%] object-cover transition-all duration-150 ease-in-out rounded-[7px] hover:transform hover:scale-105'
+          />
+        </div>
+        <h1 className='text-xl lg:text-2xl font-bold p-2'>{props.heading}</h1>
+        <h1 className='flex text-sm lg:text-base items-center gap-2 p-2'>
+          <FaMapMarker className='text-blue-800' /> {props.location}
+        </h1>
+        <p className='text-md lg:text-lg p-2'>{props.text}</p>
+        <div className='flex justify-between items-baseline p-2'>
+          <h1 className='text-red-600'>{`₹ ${props.price}`}</h1>
+          <button className='mr-2 lg:mr-5 border bg-black text-white rounded-[5px] p-2 lg:p-5'>
+            Details
+          </button>
+        </div>
       </div>
-      <h1 className='text-[25px] font-bold   p-[5px ]'>{props.heading}</h1>
-      <h1 className='flex text-[12px] items-center gap-2'><FaMapMarker className=' text-blue-800'></FaMapMarker> {props.location}</h1>
-      <p className='text-[15px] mt-[5px]'>{props.text}</p>
-      <div className='flex justify-between align-baseline'>
-        <h1 className='text-red-600 mt-[4px]'>₹  {props.price}</h1>
-        <button className='mr-[5px] border bg-black text-white rounded-[5px] p-[5px]'>Details</button>
-      </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default DestinationData
+export default DestinationData;
