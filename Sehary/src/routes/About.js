797cc624395506fb425d0e7ba9660a294932d91a
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-09-13 15:50:38 +0530

    updated About us

--- Sehary/src/routes/About.js
+++ Sehary/src/routes/About.js
@@ -18,16 +18,16 @@ const About = () => {
       btnClass="btn"
       >
       </Parent>
-
-      <h1 className='text-[65px] font-bold text-center'>Our Leadership</h1>
-      <div className='ml-[200px] mr-[200px] mb-[100px]'>
-      <p className='text-center text-md'>Join us to redefine Indian travel. Experience transformative offbeat journeys that fuse adventure and culture. Uncover hidden treasures on the road less taken. Your incredible journey begins here.</p>
+      <div className='text-center'>
+        <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold'>Our Leadership</h1>
+      </div>
+      <div className='mx-4 sm:mx-8 md:mx-16 lg:mx-24 mt-4 sm:mt-8 md:mt-16 lg:mt-24 mb-8 sm:mb-16 md:mb-32 lg:mb-64'>
+        <p className='text-center text-base sm:text-lg md:text-xl lg:text-2xl'>
+          Join us to redefine Indian travel. Experience transformative offbeat journeys that fuse adventure and culture. Uncover hidden treasures on the road less taken. Your incredible journey begins here.
+        </p>
       </div>
       <Team></Team>
-
-
-      <Footer></Footer>
-      
+      <Footer></Footer>    
     </div>
   )
 }
